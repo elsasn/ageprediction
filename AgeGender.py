2bca06297971641df9893a5984522c0f73dd9042
elsasn <elsa.snb@gmail.com>
2021-06-18 22:28:27 +0700

    Update Code

--- AgeGender.py
+++ AgeGender.py
@@ -1,14 +1,14 @@
 # Import required modules
-import cv2 as cv
+import cv22 as cv2
 import math
 import time
 import argparse
 
 def getFaceBox(net, frame, conf_threshold=0.7):
-    frameOpencvDnn = frame.copy()
-    frameHeight = frameOpencvDnn.shape[0]
-    frameWidth = frameOpencvDnn.shape[1]
-    blob = cv.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+    frameOpencv2Dnn = frame.copy()
+    frameHeight = frameOpencv2Dnn.shape[0]
+    frameWidth = frameOpencv2Dnn.shape[1]
+    blob = cv2.dnn.blobFromImage(frameOpencv2Dnn, 1.0, (300, 300), [104, 117, 123], True, False)
 
     net.setInput(blob)
     detections = net.forward()
@@ -21,11 +21,11 @@ def getFaceBox(net, frame, conf_threshold=0.7):
             x2 = int(detections[0, 0, i, 5] * frameWidth)
             y2 = int(detections[0, 0, i, 6] * frameHeight)
             bboxes.append([x1, y1, x2, y2])
-            cv.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight/150)), 8)
-    return frameOpencvDnn, bboxes
+            cv2.rectangle(frameOpencv2Dnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight/150)), 8)
+    return frameOpencv2Dnn, bboxes
 
 
-parser = argparse.ArgumentParser(description='Use this script to run age and gender recognition using OpenCV.')
+parser = argparse.ArgumentParser(description='Use this script to run age and gender recognition using Opencv2.')
 parser.add_argument('--input', help='Path to input image or video file. Skip this argument to capture frames from a camera.')
 parser.add_argument("--device", default="cpu", help="Device to inference on")
 
@@ -34,8 +34,8 @@ args = parser.parse_args()
 
 args = parser.parse_args()
 
-faceProto = "opencv_face_detector.pbtxt"
-faceModel = "opencv_face_detector_uint8.pb"
+faceProto = "opencv2_face_detector.pbtxt"
+faceModel = "opencv2_face_detector_uint8.pb"
 
 ageProto = "age_deploy.prototxt"
 ageModel = "age_net.caffemodel"
@@ -48,40 +48,40 @@ ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)
 genderList = ['Male', 'Female']
 
 # Load network
-ageNet = cv.dnn.readNet(ageModel, ageProto)
-genderNet = cv.dnn.readNet(genderModel, genderProto)
-faceNet = cv.dnn.readNet(faceModel, faceProto)
+ageNet = cv2.dnn.readNet(ageModel, ageProto)
+genderNet = cv2.dnn.readNet(genderModel, genderProto)
+faceNet = cv2.dnn.readNet(faceModel, faceProto)
 
 
 if args.device == "cpu":
-    ageNet.setPreferableBackend(cv.dnn.DNN_TARGET_CPU)
+    ageNet.setPreferableBackend(cv2.dnn.DNN_TARGET_CPU)
 
-    genderNet.setPreferableBackend(cv.dnn.DNN_TARGET_CPU)
+    genderNet.setPreferableBackend(cv2.dnn.DNN_TARGET_CPU)
     
-    faceNet.setPreferableBackend(cv.dnn.DNN_TARGET_CPU)
+    faceNet.setPreferableBackend(cv2.dnn.DNN_TARGET_CPU)
 
     print("Using CPU device")
 elif args.device == "gpu":
-    ageNet.setPreferableBackend(cv.dnn.DNN_BACKEND_CUDA)
-    ageNet.setPreferableTarget(cv.dnn.DNN_TARGET_CUDA)
+    ageNet.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+    ageNet.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
-    genderNet.setPreferableBackend(cv.dnn.DNN_BACKEND_CUDA)
-    genderNet.setPreferableTarget(cv.dnn.DNN_TARGET_CUDA)
+    genderNet.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+    genderNet.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
-    genderNet.setPreferableBackend(cv.dnn.DNN_BACKEND_CUDA)
-    genderNet.setPreferableTarget(cv.dnn.DNN_TARGET_CUDA)
+    genderNet.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+    genderNet.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
     print("Using GPU device")
 
 
 # Open a video file or an image file or a camera stream
-cap = cv.VideoCapture(args.input if args.input else 0)
+cap = cv2.VideoCapture(args.input if args.input else 0)
 padding = 20
-while cv.waitKey(1) < 0:
+while cv2.waitKey(1) < 0:
     # Read frame
     t = time.time()
     hasFrame, frame = cap.read()
     if not hasFrame:
-        cv.waitKey()
+        cv2.waitKey()
         break
 
     frameFace, bboxes = getFaceBox(faceNet, frame)
@@ -93,7 +93,7 @@ while cv.waitKey(1) < 0:
         # print(bbox)
         face = frame[max(0,bbox[1]-padding):min(bbox[3]+padding,frame.shape[0]-1),max(0,bbox[0]-padding):min(bbox[2]+padding, frame.shape[1]-1)]
 
-        blob = cv.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+        blob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
         genderNet.setInput(blob)
         genderPreds = genderNet.forward()
         gender = genderList[genderPreds[0].argmax()]
@@ -107,11 +107,11 @@ while cv.waitKey(1) < 0:
         print("Age : {}, conf = {:.3f}".format(age, agePreds[0].max()))
 
         label = "{},{}".format(gender, age)
-        cv.putText(frameFace, label, (bbox[0], bbox[1]-10), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv.LINE_AA)
-        cv.imshow("Age Gender Demo", frameFace)
-        # cv.imwrite("age-gender-out-{}".format(args.input),frameFace)
+        cv2.putText(frameFace, label, (bbox[0], bbox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv2.LINE_AA)
+        cv2.imshow("Age Gender Demo", frameFace)
+        # cv2.imwrite("age-gender-out-{}".format(args.input),frameFace)
     print("time : {:.3f}".format(time.time() - t))
 
 
  
-# cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=~/opencv_gpu -DINSTALL_PYTHON_EXAMPLES=OFF -DINSTALL_C_EXAMPLES=OFF -DOPENCV_ENABLE_NONFREE=ON -DOPENCV_EXTRA_MODULES_PATH=~/cv2_gpu/opencv_contrib/modules -DPYTHON_EXECUTABLE=~/env/bin/python3 -DBUILD_EXAMPLES=ON -DWITH_CUDA=ON -DWITH_CUDNN=ON -DOPENCV_DNN_CUDA=ON  -DENABLE_FAST_MATH=ON -DCUDA_FAST_MATH=ON  -DWITH_CUBLAS=ON -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-10.2 -DOpenCL_LIBRARY=/usr/local/cuda-10.2/lib64/libOpenCL.so -DOpenCL_INCLUDE_DIR=/usr/local/cuda-10.2/include/ ..
+# cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=~/opencv2_gpu -DINSTALL_PYTHON_EXAMPLES=OFF -DINSTALL_C_EXAMPLES=OFF -DOPENcv2_ENABLE_NONFREE=ON -DOPENcv2_EXTRA_MODULES_PATH=~/cv22_gpu/opencv2_contrib/modules -DPYTHON_EXECUTABLE=~/env/bin/python3 -DBUILD_EXAMPLES=ON -DWITH_CUDA=ON -DWITH_CUDNN=ON -DOPENcv2_DNN_CUDA=ON  -DENABLE_FAST_MATH=ON -DCUDA_FAST_MATH=ON  -DWITH_CUBLAS=ON -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-10.2 -DOpenCL_LIBRARY=/usr/local/cuda-10.2/lib64/libOpenCL.so -DOpenCL_INCLUDE_DIR=/usr/local/cuda-10.2/include/ ..
