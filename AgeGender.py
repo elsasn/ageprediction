474faf114edd6fb0fc823b7d0c0ba89084b3cd16
elsasn <elsa.snb@gmail.com>
2021-06-20 21:18:45 +0700

    Fix Code

--- AgeGender.py
+++ AgeGender.py
@@ -1,81 +1,107 @@
-# Import required modules
-import cv22 as cv2
-import math
+# SOURCES:
+# https://learnopencv.com/age-gender-classification-using-opencv-deep-learning-c-python/, model loading and usage code taken from there
+# https://discuss.streamlit.io/t/remove-made-with-streamlit-from-bottom-of-app/1370/2, Hiding the hamburger menu and watermark
+
 import time
+
+import cv2
+import numpy as np
+import streamlit as st
+from PIL import Image
 import argparse
 
-def getFaceBox(net, frame, conf_threshold=0.7):
-    frameOpencv2Dnn = frame.copy()
-    frameHeight = frameOpencv2Dnn.shape[0]
-    frameWidth = frameOpencv2Dnn.shape[1]
-    blob = cv2.dnn.blobFromImage(frameOpencv2Dnn, 1.0, (300, 300), [104, 117, 123], True, False)
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-    net.setInput(blob)
+def get_face_box(net, frame, conf_threshold=0.7):
+    opencv_dnn_frame = frame.copy()
+    frame_height = opencv_dnn_frame.shape[0]
+    frame_width = opencv_dnn_frame.shape[1]
+    blob_img = cv2.dnn.blobFromImage(opencv_dnn_frame, 1.0, (300, 300), [
+        104, 117, 123], True, False)
+
+    net.setInput(blob_img)
     detections = net.forward()
-    bboxes = []
+    b_boxes_detect = []
     for i in range(detections.shape[2]):
         confidence = detections[0, 0, i, 2]
         if confidence > conf_threshold:
-            x1 = int(detections[0, 0, i, 3] * frameWidth)
-            y1 = int(detections[0, 0, i, 4] * frameHeight)
-            x2 = int(detections[0, 0, i, 5] * frameWidth)
-            y2 = int(detections[0, 0, i, 6] * frameHeight)
-            bboxes.append([x1, y1, x2, y2])
-            cv2.rectangle(frameOpencv2Dnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight/150)), 8)
-    return frameOpencv2Dnn, bboxes
+            x1 = int(detections[0, 0, i, 3] * frame_width)
+            y1 = int(detections[0, 0, i, 4] * frame_height)
+            x2 = int(detections[0, 0, i, 5] * frame_width)
+            y2 = int(detections[0, 0, i, 6] * frame_height)
+            b_boxes_detect.append([x1, y1, x2, y2])
+            cv2.rectangle(opencv_dnn_frame, (x1, y1), (x2, y2),
+                          (0, 255, 0), int(round(frame_height / 150)), 8)
+    return opencv_dnn_frame, b_boxes_detect
 
 
-parser = argparse.ArgumentParser(description='Use this script to run age and gender recognition using Opencv2.')
-parser.add_argument('--input', help='Path to input image or video file. Skip this argument to capture frames from a camera.')
-parser.add_argument("--device", default="cpu", help="Device to inference on")
+st.write("""
+    # Age and Gender prediction
+    """)
 
-args = parser.parse_args()
+st.write("## Upload a picture that contains a face")
 
+uploaded_file = st.file_uploader("Choose a file:")
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    cap = np.array(image)
+    cv2.imwrite('temp.jpg', cv2.cvtColor(cap, cv2.COLOR_RGB2BGR))
+    cap = cv2.imread('temp.jpg')
 
-args = parser.parse_args()
+    face_txt_path = "opencv_face_detector.pbtxt"
+    face_model_path = "opencv_face_detector_uint8.pb"
 
-faceProto = "opencv2_face_detector.pbtxt"
-faceModel = "opencv2_face_detector_uint8.pb"
+    age_txt_path = "age_deploy.prototxt"
+    age_model_path = "age_net.caffemodel"
 
-ageProto = "age_deploy.prototxt"
-ageModel = "age_net.caffemodel"
+    gender_txt_path = "gender_deploy.prototxt"
+    gender_model_path = "gender_net.caffemodel"
 
-genderProto = "gender_deploy.prototxt"
-genderModel = "gender_net.caffemodel"
+    MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+    age_classes = ['(0-2)', '(4-6)', '(8-12)', '(15-20)',
+                   '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+    gender_classes = ['Male', 'Female']
 
-MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
-ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
-genderList = ['Male', 'Female']
+    age_net = cv2.dnn.readNet(age_model_path, age_txt_path)
+    gender_net = cv2.dnn.readNet(gender_model_path, gender_txt_path)
+    face_net = cv2.dnn.readNet(face_model_path, face_txt_path)
 
-# Load network
-ageNet = cv2.dnn.readNet(ageModel, ageProto)
-genderNet = cv2.dnn.readNet(genderModel, genderProto)
-faceNet = cv2.dnn.readNet(faceModel, faceProto)
+    padding = 20
+    t = time.time()
+    frameFace, b_boxes = get_face_box(face_net, cap)
+    if not b_boxes:
+        st.write("No face Detected, Checking next frame")
 
+    for bbox in b_boxes:
+        face = cap[max(0, bbox[1] - padding):min(bbox[3] + padding, cap.shape[0] - 1),
+               max(0, bbox[0] - padding): min(bbox[2] + padding, cap.shape[1] - 1)]
 
-if args.device == "cpu":
-    ageNet.setPreferableBackend(cv2.dnn.DNN_TARGET_CPU)
+        blob = cv2.dnn.blobFromImage(
+            face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+        gender_net.setInput(blob)
+        gender_pred_list = gender_net.forward()
+        gender = gender_classes[gender_pred_list[0].argmax()]
+        st.write(f"Gender : {gender}, confidence = {gender_pred_list[0].max() * 100}%")
 
-    genderNet.setPreferableBackend(cv2.dnn.DNN_TARGET_CPU)
-    
-    faceNet.setPreferableBackend(cv2.dnn.DNN_TARGET_CPU)
+        age_net.setInput(blob)
+        age_pred_list = age_net.forward()
+        age = age_classes[age_pred_list[0].argmax()]
+        st.write(f"Age : {age}, confidence = {age_pred_list[0].max() * 100}%")
 
-    print("Using CPU device")
-elif args.device == "gpu":
-    ageNet.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-    ageNet.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+        label = "{},{}".format(gender, age)
+        cv2.putText(frameFace, label, (bbox[0], bbox[1] - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv2.LINE_AA)
+        st.image(frameFace)
 
-    genderNet.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-    genderNet.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
-
-    genderNet.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-    genderNet.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
-    print("Using GPU device")
-
-
-# Open a video file or an image file or a camera stream
-cap = cv2.VideoCapture(args.input if args.input else 0)
-padding = 20
+st.write('## Or Open you Camera')
+from streamlit_webrtc import webrtc_streamer
+cap = webrtc_streamer(key="example")
 while cv2.waitKey(1) < 0:
     # Read frame
     t = time.time()
@@ -84,34 +110,48 @@ while cv2.waitKey(1) < 0:
         cv2.waitKey()
         break
 
-    frameFace, bboxes = getFaceBox(faceNet, frame)
-    if not bboxes:
-        print("No face Detected, Checking next frame")
-        continue
 
-    for bbox in bboxes:
-        # print(bbox)
-        face = frame[max(0,bbox[1]-padding):min(bbox[3]+padding,frame.shape[0]-1),max(0,bbox[0]-padding):min(bbox[2]+padding, frame.shape[1]-1)]
+    face_txt_path = "opencv_face_detector.pbtxt"
+    face_model_path = "opencv_face_detector_uint8.pb"
 
-        blob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
-        genderNet.setInput(blob)
-        genderPreds = genderNet.forward()
-        gender = genderList[genderPreds[0].argmax()]
-        # print("Gender Output : {}".format(genderPreds))
-        print("Gender : {}, conf = {:.3f}".format(gender, genderPreds[0].max()))
+    age_txt_path = "age_deploy.prototxt"
+    age_model_path = "age_net.caffemodel"
 
-        ageNet.setInput(blob)
-        agePreds = ageNet.forward()
-        age = ageList[agePreds[0].argmax()]
-        print("Age Output : {}".format(agePreds))
-        print("Age : {}, conf = {:.3f}".format(age, agePreds[0].max()))
+    gender_txt_path = "gender_deploy.prototxt"
+    gender_model_path = "gender_net.caffemodel"
+
+    MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+    age_classes = ['(0-2)', '(4-6)', '(8-12)', '(15-20)',
+                   '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+    gender_classes = ['Male', 'Female']
+
+    age_net = cv2.dnn.readNet(age_model_path, age_txt_path)
+    gender_net = cv2.dnn.readNet(gender_model_path, gender_txt_path)
+    face_net = cv2.dnn.readNet(face_model_path, face_txt_path)
+
+    padding = 20
+    t = time.time()
+    frameFace, b_boxes = get_face_box(face_net, cap)
+    if not b_boxes:
+        st.write("No face Detected, Checking next frame")
+
+    for bbox in b_boxes:
+        face = cap[max(0, bbox[1] - padding):min(bbox[3] + padding, cap.shape[0] - 1),
+               max(0, bbox[0] - padding): min(bbox[2] + padding, cap.shape[1] - 1)]
+
+        blob = cv2.dnn.blobFromImage(
+            face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+        gender_net.setInput(blob)
+        gender_pred_list = gender_net.forward()
+        gender = gender_classes[gender_pred_list[0].argmax()]
+        st.write(f"Gender : {gender}, confidence = {gender_pred_list[0].max() * 100}%")
+
+        age_net.setInput(blob)
+        age_pred_list = age_net.forward()
+        age = age_classes[age_pred_list[0].argmax()]
+        st.write(f"Age : {age}, confidence = {age_pred_list[0].max() * 100}%")
 
         label = "{},{}".format(gender, age)
-        cv2.putText(frameFace, label, (bbox[0], bbox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv2.LINE_AA)
-        cv2.imshow("Age Gender Demo", frameFace)
-        # cv2.imwrite("age-gender-out-{}".format(args.input),frameFace)
-    print("time : {:.3f}".format(time.time() - t))
-
-
- 
-# cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=~/opencv2_gpu -DINSTALL_PYTHON_EXAMPLES=OFF -DINSTALL_C_EXAMPLES=OFF -DOPENcv2_ENABLE_NONFREE=ON -DOPENcv2_EXTRA_MODULES_PATH=~/cv22_gpu/opencv2_contrib/modules -DPYTHON_EXECUTABLE=~/env/bin/python3 -DBUILD_EXAMPLES=ON -DWITH_CUDA=ON -DWITH_CUDNN=ON -DOPENcv2_DNN_CUDA=ON  -DENABLE_FAST_MATH=ON -DCUDA_FAST_MATH=ON  -DWITH_CUBLAS=ON -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-10.2 -DOpenCL_LIBRARY=/usr/local/cuda-10.2/lib64/libOpenCL.so -DOpenCL_INCLUDE_DIR=/usr/local/cuda-10.2/include/ ..
+        cv2.putText(frameFace, label, (bbox[0], bbox[1] - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv2.LINE_AA)
+        st.image(frameFace)
